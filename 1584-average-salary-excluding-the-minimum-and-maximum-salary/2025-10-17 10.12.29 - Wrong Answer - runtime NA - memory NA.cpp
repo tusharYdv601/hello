157b2eb9117e1238e96adba1086baf04d6ea18d1
tusharYdv601 <tushar.yadav2777@gmail.com>
2025-10-17 10:12:29 +0530

    Runtime: 0 ms, Memory: 0 MB

---
+++ 1584-average-salary-excluding-the-minimum-and-maximum-salary/2025-10-17 10.12.29 - Wrong Answer - runtime NA - memory NA.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    double average(vector<int>& salary) {
+       int min= *std::min_element(salary.begin(), salary.end());
+        int max = *std::max_element(salary.begin(), salary.end());
+        int n=salary.size();
+        int sum=0;
+        int total_sum = std::accumulate(salary.begin(), salary.end(), sum);
+        double avg=(total_sum-max-min)/(n-2);
+        return avg;
+    }
+
+};
