0ffc5ec568692de6e12ee3e3f9380028f9a7ee16
tusharYdv601 <tushar.yadav2777@gmail.com>
2025-10-17 23:50:57 +0530

    Runtime: 0 ms, Memory: 0 MB

---
+++ 189-rotate-array/2025-10-17 23.50.57 - Runtime Error - runtime NA - memory NA.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        std::reverse(nums.begin(), nums.end());
+        
+       
+        std::reverse(nums.begin(), nums.begin() + k);
+        
+       
+        std::reverse(nums.begin() + k, nums.end());
+    }
+};
