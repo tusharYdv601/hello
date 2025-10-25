3f35676bbb6680ba2c8fb2ce702aa8c264597cc0
tusharYdv601 <tushar.yadav2777@gmail.com>
2025-10-25 19:41:22 +0530

    Runtime: 0 ms, Memory: 23.9 MB

---
+++ 283-move-zeroes/2025-10-25 19.41.22 - Accepted - runtime 0ms - memory 23.9MB.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int slow = 0;
+
+        for (int fast = 0; fast < nums.size(); ++fast) {
+
+            if (nums[fast] != 0) {
+
+                std::swap(nums[slow], nums[fast]);
+
+                slow++;
+            }
+        }
+    }
+};
