4dc2de0f088a1834592052fa7e9e7172dded25ec
tusharYdv601 <tushar.yadav2777@gmail.com>
2025-11-01 21:17:00 +0530

    Runtime: 3 ms, Memory: 12.2 MB

---
+++ 12-integer-to-roman/2025-11-01 21.17.00 - Accepted - runtime 3ms - memory 12.2MB.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        vector<int> values = {
+            1000, 900, 500, 400, 100, 
+            90, 50, 40, 10, 9, 5, 4, 1
+        };
+        vector<string> symbols = {
+            "M", "CM", "D", "CD", "C", 
+            "XC", "L", "XL", "X", "IX", "V", "IV", "I"
+        };
+        string romanNumeral = "";
+        for (int i = 0; i < values.size(); ++i) {
+            while (num >= values[i]) {
+                romanNumeral += symbols[i];
+                num -= values[i];
+            }
+
+            if (num == 0) {
+                break;
+            }
+        }
+        return romanNumeral;
+    }
+};
