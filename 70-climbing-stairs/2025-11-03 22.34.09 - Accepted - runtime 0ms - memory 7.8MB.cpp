45be437d743145be1f48ae74c6762285f5e5a15f
tusharYdv601 <tushar.yadav2777@gmail.com>
2025-11-03 22:34:09 +0530

    Runtime: 0 ms, Memory: 7.8 MB

---
+++ 70-climbing-stairs/2025-11-03 22.34.09 - Accepted - runtime 0ms - memory 7.8MB.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+         if (n == 1) return 1;
+        
+        int a = 1;
+        int b = 2; 
+        
+        for (int i = 3; i <= n; i++) {
+            int current = a + b;
+            a = b;         
+            b = current;   
+        }
+        return b;
+    }
+    
+};
