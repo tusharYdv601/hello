632476e427f41a8784ba5d1c7bd0a72363a488e5
tusharYdv601 <tushar.yadav2777@gmail.com>
2025-11-04 15:20:43 +0530

    Runtime: 0 ms, Memory: 7.7 MB

---
+++ 168-excel-sheet-column-title/2025-11-04 15.20.43 - Accepted - runtime 0ms - memory 7.7MB.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string result = "";
+        
+        while (columnNumber > 0) {
+             columnNumber--; 
+            
+            
+            int rem = columnNumber % 26;
+            
+            result = (char)('A' + rem) + result;
+            
+            
+            columnNumber /= 26;
+        }
+        
+        return result;
+    }
+};
