fd6c03ca7517c05143954d8574bdb9be4f407eaf
tusharYdv601 <tushar.yadav2777@gmail.com>
2025-11-07 09:06:09 +0530

    Runtime: 12 ms, Memory: 44.9 MB

---
+++ 455-assign-cookies/2025-11-07 09.06.09 - Accepted - runtime 12ms - memory 44.9MB.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        std::sort(g.begin(), g.end());
+        std::sort(s.begin(), s.end());
+
+        int child_index = 0;
+        int cookie_index = 0;
+        int content_children = 0;
+
+        while (child_index < g.size() && cookie_index < s.size()) {
+
+            if (s[cookie_index] >= g[child_index]) {
+                content_children++;
+                child_index++;
+                cookie_index++;
+            } else {
+                cookie_index++;
+            }
+        }
+
+        return content_children;
+    }
+};
