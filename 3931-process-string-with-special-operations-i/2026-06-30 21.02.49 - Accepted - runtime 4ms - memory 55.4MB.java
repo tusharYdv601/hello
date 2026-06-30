09293350bc211104afc5cbe64980b4d5060a7ffa
tusharYdv601 <tushar.yadav2777@gmail.com>
2026-06-30 21:02:49 +0530

    Runtime: 4 ms, Memory: 55.4 MB

---
+++ 3931-process-string-with-special-operations-i/2026-06-30 21.02.49 - Accepted - runtime 4ms - memory 55.4MB.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (c == '%') {
+                sb.reverse();
+            } else if (c == '*') {
+                if (sb.length() > 0) {
+                    sb.deleteCharAt(sb.length() - 1);
+                }
+            } else if (c == '#') {
+                 sb.append(sb.toString());
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+
+    }
+}
