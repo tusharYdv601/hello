3b5b243381bc92912aaf5e6256d3134e8242b57c
tusharYdv601 <tushar.yadav2777@gmail.com>
2026-07-04 20:57:12 +0530

    Runtime: 8 ms, Memory: 42.9 MB

---
+++ 647-palindromic-substrings/2026-07-04 20.57.12 - Accepted - runtime 8ms - memory 42.9MB.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int countSubstrings(String s) {
+        return axisOrbit(s);
+
+    }
+
+    public static int axisOrbit(String s) {
+        int odd = 0;
+        for (int axis = 0; axis < s.length(); axis++) {
+            for (int orbit = 0; axis - orbit >= 0 && axis + orbit < s.length(); orbit++) {
+                if (s.charAt(axis - orbit) != s.charAt(axis + orbit)) {
+                    break;
+                }
+                odd++;
+            }
+        }
+        int even = 0;
+        for (double axis = 0.5; axis < s.length(); axis++) {
+            for (double orbit = 0.5; axis - orbit >= 0 && axis + orbit < s.length(); orbit++) {
+                if (s.charAt((int) (axis - orbit)) != s.charAt((int) (axis + orbit))) {
+                    break;
+                }
+                even++;
+            }
+        }
+        return odd + even;
+    }
+}
