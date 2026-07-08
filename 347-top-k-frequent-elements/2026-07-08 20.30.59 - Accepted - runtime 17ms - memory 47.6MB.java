2f327abaf39f6cbccb711c50c2bdf5b8cfb90e0f
tusharYdv601 <tushar.yadav2777@gmail.com>
2026-07-08 20:30:59 +0530

    Runtime: 17 ms, Memory: 47.6 MB

---
+++ 347-top-k-frequent-elements/2026-07-08 20.30.59 - Accepted - runtime 17ms - memory 47.6MB.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>((n1, n2) -> map.get(n1) - map.get(n2));
+
+        for (int num : map.keySet()) {
+            minHeap.add(num);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+
+        int[] result = new int[k];
+        for (int i = 0; i < k; i++) {
+            result[i] = minHeap.poll();
+        }
+        return result;
+
+    }
+}
