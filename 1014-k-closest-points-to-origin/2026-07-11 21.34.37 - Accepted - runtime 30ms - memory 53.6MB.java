6bcb34b13fa9688d71c7ff95900e094a801a5fe6
tusharYdv601 <tushar.yadav2777@gmail.com>
2026-07-11 21:34:37 +0530

    Runtime: 30 ms, Memory: 53.6 MB

---
+++ 1014-k-closest-points-to-origin/2026-07-11 21.34.37 - Accepted - runtime 30ms - memory 53.6MB.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>(
+                (p1, p2) -> (p2[0] * p2[0] + p2[1] * p2[1]) - (p1[0] * p1[0] + p1[1] * p1[1]));
+        for (int[] point : points) {
+            maxHeap.offer(point);
+
+            if (maxHeap.size() > k) {
+                maxHeap.poll();
+            }
+        }
+        for (int i = k; i < points.length; i++) {
+
+        }
+        int[][] result = new int[k][2];
+        for (int i = 0; i < k; i++) {
+            result[i] = maxHeap.poll();
+        }
+
+        return result;
+
+    }
+}
