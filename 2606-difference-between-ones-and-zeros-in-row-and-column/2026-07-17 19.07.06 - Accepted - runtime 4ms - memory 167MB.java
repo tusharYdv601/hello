69b14449e4ff0b131427e12efe7835a2d9112d26
tusharYdv601 <tushar.yadav2777@gmail.com>
2026-07-17 19:07:06 +0530

    Runtime: 4 ms, Memory: 167 MB

---
+++ 2606-difference-between-ones-and-zeros-in-row-and-column/2026-07-17 19.07.06 - Accepted - runtime 4ms - memory 167MB.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[][] onesMinusZeros(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int[] onesRow = new int[m];
+        int[] onesCol = new int[n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                onesRow[i] += grid[i][j];
+                onesCol[j] += grid[i][j];
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = 2 * onesRow[i] + 2 * onesCol[j] - m - n;
+            }
+        }
+
+        return grid;
+
+    }
+}
