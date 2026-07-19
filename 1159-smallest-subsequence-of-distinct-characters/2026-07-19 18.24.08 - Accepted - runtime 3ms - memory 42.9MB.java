318cf1abb9ae9b345a73cfed679ee17fdd2f69fb
tusharYdv601 <tushar.yadav2777@gmail.com>
2026-07-19 18:24:08 +0530

    Runtime: 3 ms, Memory: 42.9 MB

---
+++ 1159-smallest-subsequence-of-distinct-characters/2026-07-19 18.24.08 - Accepted - runtime 3ms - memory 42.9MB.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String smallestSubsequence(String s) {
+        int[] idx = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            idx[s.charAt(i) - 'a'] = i;
+        }
+        boolean[] seen = new boolean[26];
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (seen[c - 'a']) {
+                continue;
+            }
+            while (!st.isEmpty() && c < st.peek() && idx[st.peek() - 'a'] > i) {
+                seen[st.pop() - 'a'] = false;
+            }
+            st.push(c);
+            seen[c - 'a'] = true;
+
+        }
+        StringBuilder sb = new StringBuilder();
+        for (char c : st) {
+            sb.append(c);
+        }
+        
+        return sb.toString();
+    }
+}
