352adfcce8c1aa9baebea3cbd5bccacd21a0e93c
tusharYdv601 <tushar.yadav2777@gmail.com>
2026-07-28 18:36:02 +0530

    Runtime: 2 ms, Memory: 48 MB

---
+++ 55-jump-game/2026-07-28 18.36.02 - Accepted - runtime 2ms - memory 48MB.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int reach=0;
+        for( int i=0; i<nums.length;i++){
+            if(i>reach){
+                return false;
+                
+            }
+            reach = Math.max(reach, i + nums[i]);
+            if(reach>=nums.length-1){
+                return true;
+            }
+        }
+        return true;
+        
+    }
+}
