dbf066f9d2750c17a07aba3c79b5fa2bdd3d5cb4
tusharYdv601 <tushar.yadav2777@gmail.com>
2026-07-29 20:22:49 +0530

    Runtime: 13 ms, Memory: 48.2 MB

---
+++ 2077-largest-number-after-mutating-substring/2026-07-29 20.22.49 - Accepted - runtime 13ms - memory 48.2MB.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String maximumNumber(String num, int[] change) {
+        StringBuilder str = new StringBuilder();
+        boolean check = false;
+        int i = 0;
+        for (i = 0; i < num.length(); i++) {
+            int n = num.charAt(i) - '0';
+            if (change[n] > n) {
+                str.append(change[n]);
+                check = true;
+            } else if (change[n] == n && check) {
+                str.append(change[n]);
+            } else if (change[n] < n && check) {
+                break;
+            }else {
+                str.append(n);
+            }
+        }
+        return str.append(num.substring(i)).toString();
+    }
+}
