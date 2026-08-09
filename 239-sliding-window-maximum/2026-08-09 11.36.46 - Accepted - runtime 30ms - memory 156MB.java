9940450b812c677084e562fa19d8eb69572eae43
tusharYdv601 <tushar.yadav2777@gmail.com>
2026-08-09 11:36:46 +0530

    Runtime: 30 ms, Memory: 156 MB

---
+++ 239-sliding-window-maximum/2026-08-09 11.36.46 - Accepted - runtime 30ms - memory 156MB.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] arr, int k) {
+         Deque<Integer> dq = new LinkedList<>();
+        int[] ans = new int[arr.length - k + 1];
+        int j = 0;
+        for (int i = 0; i < k; i++) {
+            while (!dq.isEmpty() && arr[i] > arr[dq.getLast()]) {
+                dq.removeLast();
+            }
+            dq.add(i);
+        }
+        ans[j++] = arr[dq.getFirst()];
+        for (int i = k; i < arr.length; i++) {
+
+            while (!dq.isEmpty() && arr[i] > arr[dq.getLast()]) {
+                dq.removeLast();
+            }
+            dq.add(i);
+            if (dq.getFirst() == i - k) {
+                dq.removeFirst();
+            }
+            ans[j++] = arr[dq.getFirst()];
+        }
+        return ans;
+    }
+}
